cfe2e58d128c2b2fc08a7ea6abb97d80ccd8deb8
Brad Shapcott <brad@nthrald.com>
2023-05-10 17:18:58 +0000

    OK commiting the shell script changes for real this time.

--- create-show-block-context-plugin.sh
+++ create-show-block-context-plugin.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# Memorializes the script run to create the plugin.  Does not need to be executed again.
+
+# Create the pluging from the plugins directory.
+cd ..
 sudo npx @wordpress/create-block show-block-context \
      --template dynamic \
      --namespace nthrald-custom \
